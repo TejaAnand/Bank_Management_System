b92d3dbd9b9cc6045aa1a98b5ed14306fef4b35d
Pedapudi Teja Anand <139356259+TejaAnand@users.noreply.github.com>
2025-05-11 12:56:06 +0530

    add_transaction.c

--- add_transaction.c
+++ add_transaction.c
@@ -3,16 +3,19 @@ extern Account* head;
 
 void add_transaction(Account* acc, TransactionType type, float amount)
 {
-    if (acc->transaction_count < MAX_TRANSACTIONS) {
+    if (acc->transaction_count < MAX_TRANSACTIONS)
+    {
         Transaction* t = &acc->transactions[acc->transaction_count++];
         t->id = generate_transaction_id();
         t->type = type;
         t->amount = amount;
-    } else {
+    }
+    else
+    {
         // shift left to remove oldest
         for (int i = 1; i < MAX_TRANSACTIONS; i++) {
             acc->transactions[i-1] = acc->transactions[i];
-        }
+    }
         Transaction* t = &acc->transactions[MAX_TRANSACTIONS-1];
         t->id = generate_transaction_id();
         t->type = type;
